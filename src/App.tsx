d2541d1fa5c77dd65e1e5a9396e5298f42ac7194
Rustam Gilyaziev <gilaziev@gmail.com>
2019-04-11 20:19:44 +0300

    add submodule

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import { IProps } from '../b/types'
+
+class App extends Component<IProps> {
   render() {
     return (
       <div className="App">
